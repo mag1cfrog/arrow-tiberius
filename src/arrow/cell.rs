fc80ecf4f8851f36f8e2064cd5435c545ed8b9fa
mag1cfrog <harrywong2017@gmail.com>
2026-05-17 18:26:19 -0700

    refactor: move arrow cell extraction

--- src/arrow/cell.rs
+++ src/arrow/cell.rs
@@ -1,6 +1,16 @@
 //! Arrow runtime cell values.
 
+use arrow_array::{
+    Array, BinaryArray, BooleanArray, Date32Array, Date64Array, Decimal32Array, Decimal64Array,
+    Decimal128Array, Decimal256Array, Float32Array, Float64Array, Int8Array, Int16Array,
+    Int32Array, Int64Array, LargeBinaryArray, LargeStringArray, StringArray,
+    TimestampMicrosecondArray, TimestampMillisecondArray, TimestampNanosecondArray,
+    TimestampSecondArray, UInt8Array, UInt16Array, UInt32Array, UInt64Array,
+};
 use arrow_buffer::i256;
+use arrow_schema::{DataType, TimeUnit};
+
+use crate::{Diagnostic, DiagnosticCode, DiagnosticSet, FieldRef, Result, SchemaMapping};
 
 /// Borrowed value extracted from one Arrow array cell.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -53,4 +63,176 @@ pub(crate) enum ArrowCell<'a> {
     Utf8(&'a str),
     /// Arrow binary value.
     Binary(&'a [u8]),
+}
+
+pub(crate) fn extract_arrow_cell<'a>(
+    array: &'a dyn Array,
+    mapping: &SchemaMapping,
+    row_index: usize,
+) -> Result<ArrowCell<'a>> {
+    if array.is_null(row_index) {
+        return Ok(ArrowCell::Null);
+    }
+
+    match mapping.arrow().data_type() {
+        DataType::Boolean => {
+            let array = downcast_array::<BooleanArray>(array, mapping, row_index)?;
+            Ok(ArrowCell::Boolean(array.value(row_index)))
+        }
+        DataType::Int8 => {
+            let array = downcast_array::<Int8Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Int8(array.value(row_index)))
+        }
+        DataType::Int16 => {
+            let array = downcast_array::<Int16Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Int16(array.value(row_index)))
+        }
+        DataType::Int32 => {
+            let array = downcast_array::<Int32Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Int32(array.value(row_index)))
+        }
+        DataType::Int64 => {
+            let array = downcast_array::<Int64Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Int64(array.value(row_index)))
+        }
+        DataType::UInt8 => {
+            let array = downcast_array::<UInt8Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::UInt8(array.value(row_index)))
+        }
+        DataType::UInt16 => {
+            let array = downcast_array::<UInt16Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::UInt16(array.value(row_index)))
+        }
+        DataType::UInt32 => {
+            let array = downcast_array::<UInt32Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::UInt32(array.value(row_index)))
+        }
+        DataType::UInt64 => {
+            let array = downcast_array::<UInt64Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::UInt64(array.value(row_index)))
+        }
+        DataType::Decimal32(_, _) => {
+            let array = downcast_array::<Decimal32Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Decimal32(array.value(row_index)))
+        }
+        DataType::Decimal64(_, _) => {
+            let array = downcast_array::<Decimal64Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Decimal64(array.value(row_index)))
+        }
+        DataType::Decimal128(_, _) => {
+            let array = downcast_array::<Decimal128Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Decimal128(array.value(row_index)))
+        }
+        DataType::Decimal256(_, _) => {
+            let array = downcast_array::<Decimal256Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Decimal256(array.value(row_index)))
+        }
+        DataType::Date32 => {
+            let array = downcast_array::<Date32Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Date32(array.value(row_index)))
+        }
+        DataType::Date64 => {
+            let array = downcast_array::<Date64Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Date64(array.value(row_index)))
+        }
+        DataType::Timestamp(time_unit, _) => match time_unit {
+            TimeUnit::Second => {
+                let array = downcast_array::<TimestampSecondArray>(array, mapping, row_index)?;
+                Ok(ArrowCell::TimestampSecond(array.value(row_index)))
+            }
+            TimeUnit::Millisecond => {
+                let array = downcast_array::<TimestampMillisecondArray>(array, mapping, row_index)?;
+                Ok(ArrowCell::TimestampMillisecond(array.value(row_index)))
+            }
+            TimeUnit::Microsecond => {
+                let array = downcast_array::<TimestampMicrosecondArray>(array, mapping, row_index)?;
+                Ok(ArrowCell::TimestampMicrosecond(array.value(row_index)))
+            }
+            TimeUnit::Nanosecond => {
+                let array = downcast_array::<TimestampNanosecondArray>(array, mapping, row_index)?;
+                Ok(ArrowCell::TimestampNanosecond(array.value(row_index)))
+            }
+        },
+        DataType::Float32 => {
+            let array = downcast_array::<Float32Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Float32(array.value(row_index)))
+        }
+        DataType::Float64 => {
+            let array = downcast_array::<Float64Array>(array, mapping, row_index)?;
+            Ok(ArrowCell::Float64(array.value(row_index)))
+        }
+        DataType::Utf8 => {
+            let array = downcast_array::<StringArray>(array, mapping, row_index)?;
+            Ok(ArrowCell::Utf8(array.value(row_index)))
+        }
+        DataType::LargeUtf8 => {
+            let array = downcast_array::<LargeStringArray>(array, mapping, row_index)?;
+            Ok(ArrowCell::Utf8(array.value(row_index)))
+        }
+        DataType::Binary => {
+            let array = downcast_array::<BinaryArray>(array, mapping, row_index)?;
+            Ok(ArrowCell::Binary(array.value(row_index)))
+        }
+        DataType::LargeBinary => {
+            let array = downcast_array::<LargeBinaryArray>(array, mapping, row_index)?;
+            Ok(ArrowCell::Binary(array.value(row_index)))
+        }
+        other => Err(unsupported_value_conversion(
+            mapping,
+            row_index,
+            format!("Arrow value extraction for {other} is not supported yet"),
+        )),
+    }
+}
+
+fn downcast_array<'a, T: Array + 'static>(
+    array: &'a dyn Array,
+    mapping: &SchemaMapping,
+    row_index: usize,
+) -> Result<&'a T> {
+    array.as_any().downcast_ref::<T>().ok_or_else(|| {
+        value_conversion_error(row_mapping_diagnostic(
+            mapping,
+            row_index,
+            DiagnosticCode::ValueTypeMismatch,
+            format!(
+                "runtime Arrow type {} does not match planned Arrow type {}",
+                array.data_type(),
+                mapping.arrow().data_type()
+            ),
+        ))
+    })
+}
+
+fn unsupported_value_conversion(
+    mapping: &SchemaMapping,
+    row_index: usize,
+    message: impl Into<String>,
+) -> crate::Error {
+    value_conversion_error(row_mapping_diagnostic(
+        mapping,
+        row_index,
+        DiagnosticCode::ValueConversionUnsupported,
+        message,
+    ))
+}
+
+fn row_mapping_diagnostic(
+    mapping: &SchemaMapping,
+    row_index: usize,
+    code: DiagnosticCode,
+    message: impl Into<String>,
+) -> Diagnostic {
+    Diagnostic::error(code, message)
+        .with_field(FieldRef::new(
+            mapping.arrow().index(),
+            mapping.arrow().name(),
+        ))
+        .with_row(row_index)
+}
+
+fn value_conversion_error(diagnostic: Diagnostic) -> crate::Error {
+    crate::Error::ValueConversion {
+        diagnostics: DiagnosticSet::from(vec![diagnostic]),
+    }
 }
--- src/write/record_batch.rs
+++ src/write/record_batch.rs
@@ -1,19 +1,13 @@
 //! Runtime record batch view and Arrow-to-MSSQL semantic conversion.
 
-use arrow_array::{
-    Array, BinaryArray, BooleanArray, Date32Array, Date64Array, Decimal32Array, Decimal64Array,
-    Decimal128Array, Decimal256Array, Float32Array, Float64Array, Int8Array, Int16Array,
-    Int32Array, Int64Array, LargeBinaryArray, LargeStringArray, RecordBatch, StringArray,
-    TimestampMicrosecondArray, TimestampMillisecondArray, TimestampNanosecondArray,
-    TimestampSecondArray, UInt8Array, UInt16Array, UInt32Array, UInt64Array, timezone::Tz,
-};
+use arrow_array::{Array, RecordBatch, timezone::Tz};
 use arrow_schema::{DataType, TimeUnit};
 use chrono::{Offset, TimeZone};
 
 use crate::{
     Diagnostic, DiagnosticCode, DiagnosticSet, FieldRef, MssqlType, MssqlTypeLength,
     NanosecondPolicy, PlanOptions, Result, SchemaMapping,
-    arrow::cell::ArrowCell,
+    arrow::cell::{ArrowCell, extract_arrow_cell},
     mssql::cell::{
         MssqlCell, MssqlDate, MssqlDateTime2, MssqlDateTimeOffset, MssqlDecimal, MssqlTime,
     },
@@ -153,126 +147,6 @@ impl<'a> RecordBatchView<'a> {
         }
 
         Ok(cells)
-    }
-}
-
-fn extract_arrow_cell<'a>(
-    array: &'a dyn Array,
-    mapping: &SchemaMapping,
-    row_index: usize,
-) -> Result<ArrowCell<'a>> {
-    if array.is_null(row_index) {
-        return Ok(ArrowCell::Null);
-    }
-
-    match mapping.arrow().data_type() {
-        DataType::Boolean => {
-            let array = downcast_array::<BooleanArray>(array, mapping, row_index)?;
-            Ok(ArrowCell::Boolean(array.value(row_index)))
-        }
-        DataType::Int8 => {
-            let array = downcast_array::<Int8Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Int8(array.value(row_index)))
-        }
-        DataType::Int16 => {
-            let array = downcast_array::<Int16Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Int16(array.value(row_index)))
-        }
-        DataType::Int32 => {
-            let array = downcast_array::<Int32Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Int32(array.value(row_index)))
-        }
-        DataType::Int64 => {
-            let array = downcast_array::<Int64Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Int64(array.value(row_index)))
-        }
-        DataType::UInt8 => {
-            let array = downcast_array::<UInt8Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::UInt8(array.value(row_index)))
-        }
-        DataType::UInt16 => {
-            let array = downcast_array::<UInt16Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::UInt16(array.value(row_index)))
-        }
-        DataType::UInt32 => {
-            let array = downcast_array::<UInt32Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::UInt32(array.value(row_index)))
-        }
-        DataType::UInt64 => {
-            let array = downcast_array::<UInt64Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::UInt64(array.value(row_index)))
-        }
-        DataType::Decimal32(_, _) => {
-            let array = downcast_array::<Decimal32Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Decimal32(array.value(row_index)))
-        }
-        DataType::Decimal64(_, _) => {
-            let array = downcast_array::<Decimal64Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Decimal64(array.value(row_index)))
-        }
-        DataType::Decimal128(_, _) => {
-            let array = downcast_array::<Decimal128Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Decimal128(array.value(row_index)))
-        }
-        DataType::Decimal256(_, _) => {
-            let array = downcast_array::<Decimal256Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Decimal256(array.value(row_index)))
-        }
-        DataType::Date32 => {
-            let array = downcast_array::<Date32Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Date32(array.value(row_index)))
-        }
-        DataType::Date64 => {
-            let array = downcast_array::<Date64Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Date64(array.value(row_index)))
-        }
-        DataType::Timestamp(time_unit, _) => match time_unit {
-            TimeUnit::Second => {
-                let array = downcast_array::<TimestampSecondArray>(array, mapping, row_index)?;
-                Ok(ArrowCell::TimestampSecond(array.value(row_index)))
-            }
-            TimeUnit::Millisecond => {
-                let array = downcast_array::<TimestampMillisecondArray>(array, mapping, row_index)?;
-                Ok(ArrowCell::TimestampMillisecond(array.value(row_index)))
-            }
-            TimeUnit::Microsecond => {
-                let array = downcast_array::<TimestampMicrosecondArray>(array, mapping, row_index)?;
-                Ok(ArrowCell::TimestampMicrosecond(array.value(row_index)))
-            }
-            TimeUnit::Nanosecond => {
-                let array = downcast_array::<TimestampNanosecondArray>(array, mapping, row_index)?;
-                Ok(ArrowCell::TimestampNanosecond(array.value(row_index)))
-            }
-        },
-        DataType::Float32 => {
-            let array = downcast_array::<Float32Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Float32(array.value(row_index)))
-        }
-        DataType::Float64 => {
-            let array = downcast_array::<Float64Array>(array, mapping, row_index)?;
-            Ok(ArrowCell::Float64(array.value(row_index)))
-        }
-        DataType::Utf8 => {
-            let array = downcast_array::<StringArray>(array, mapping, row_index)?;
-            Ok(ArrowCell::Utf8(array.value(row_index)))
-        }
-        DataType::LargeUtf8 => {
-            let array = downcast_array::<LargeStringArray>(array, mapping, row_index)?;
-            Ok(ArrowCell::Utf8(array.value(row_index)))
-        }
-        DataType::Binary => {
-            let array = downcast_array::<BinaryArray>(array, mapping, row_index)?;
-            Ok(ArrowCell::Binary(array.value(row_index)))
-        }
-        DataType::LargeBinary => {
-            let array = downcast_array::<LargeBinaryArray>(array, mapping, row_index)?;
-            Ok(ArrowCell::Binary(array.value(row_index)))
-        }
-        other => Err(unsupported_value_conversion(
-            mapping,
-            row_index,
-            format!("Arrow value extraction for {other} is not supported yet"),
-        )),
     }
 }
 
@@ -1555,25 +1429,6 @@ fn exceeds_length(length: MssqlTypeLength, actual: usize) -> bool {
         MssqlTypeLength::Bounded(limit) => actual > limit,
         MssqlTypeLength::Max => false,
     }
-}
-
-fn downcast_array<'a, T: Array + 'static>(
-    array: &'a dyn Array,
-    mapping: &SchemaMapping,
-    row_index: usize,
-) -> Result<&'a T> {
-    array.as_any().downcast_ref::<T>().ok_or_else(|| {
-        value_conversion_error(row_mapping_diagnostic(
-            mapping,
-            row_index,
-            DiagnosticCode::ValueTypeMismatch,
-            format!(
-                "runtime Arrow type {} does not match planned Arrow type {}",
-                array.data_type(),
-                mapping.arrow().data_type()
-            ),
-        ))
-    })
 }
 
 fn unsupported_timezone_error(
